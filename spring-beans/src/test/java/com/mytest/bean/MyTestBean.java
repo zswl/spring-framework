320c9aa89508ee1fab597eb47d0412fe468a201b
SHARE <zswl_2013@163.com>
2015-12-11 10:29:30 +0800

    新增测试文件

---
+++ spring-beans/src/test/java/com/mytest/bean/MyTestBean.java
@@ -0,0 +1,16 @@
+package com.mytest.bean;
+
+/**
+ * Created by SHARE on 2015-10-26.
+ */
+public class MyTestBean {
+	private String testStr = "testStr";
+
+	public String getTestStr() {
+		return testStr;
+	}
+
+	public void setTestStr(String testStr) {
+		this.testStr = testStr;
+	}
+}
