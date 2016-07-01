1733ad75d4f989aa1a479682f91ef63f732f89ec
SHARE <zswl_2013@163.com>
2016-07-01 16:37:24 +0800

    1

---
+++ spring-beans/src/test/java/com/mytest/BeanFactoryTest.java
@@ -0,0 +1,40 @@
+package com.mytest;
+
+import com.mytest.bean.MyTestBean;
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * 用于测试XmlBeanFactory的测试类，
+ * spring源码学习第一步
+ * 学习目标：1.spring怎样加载xml
+ * 			2.spring怎样解析bean
+ * 			3.spring怎样注册bean
+ * 			4.spring怎样反射实例化bean类
+ * Created by zswl on 2015-10-26.
+ */
+public class BeanFactoryTest {
+	@Test
+	public void testSimpleLoad() {
+		/**
+		 * 使用资源路径加载器加载xml文件
+		 */
+		ClassPathResource classPathResource = new ClassPathResource("bean/beanFactoryTest.xml");
+		/**
+		 * 根据资源创建bean工厂
+		 */
+		BeanFactory bf = new XmlBeanFactory(classPathResource);
+		/**
+		 * 使用bean反射实例化bean
+		 */
+		MyTestBean bean = (MyTestBean) bf.getBean("myTestBean");
+		/**
+		 * Junit执行实例化的bean类方法
+		 */
+		assertEquals("testStr", bean.getTestStr());
+	}
+}
